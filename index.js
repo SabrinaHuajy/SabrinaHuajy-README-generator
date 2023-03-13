d87f63618b7209d572c672e21eef57ccfcb103eb
SabrinaHuajy <sabrinahua@icloud.com>
2023-03-13 08:57:50 +0800

    updated readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 const util = require("util");
 const writeToFile = util.promisify(fs.writeFile);
 
@@ -71,8 +71,8 @@ function promptUser() {
 
 
 // function to write README file
-// function writeToFile(fileName, data) {
-// }
+function writeToFile(dist, data) {
+}
 
 
 function generateMD(answers) {
